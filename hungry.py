89afca43faa138a606e34538195f8d40bb8a66ab
bharathbhimshetty <bharathbhimshetty@gmsil.com>
2020-09-28 18:09:25 +0530

    added pizza burger and fries

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@ hungry = input('Are you Hungry?')
 
 if hungry == 'yes':
 	print('Eat Samosa')
-	print('Eat Pizzaaaaa')
+	print('Eat Pizza')
+	print('Eat Burger')
+	print('Eat Fries')
 else:
 	print('Keep working')
